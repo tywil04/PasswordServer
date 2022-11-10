ceabefa77bce7aff6434d0035d4704916b15f7ff
Tyler <68185249+tywil04@users.noreply.github.com>
2022-11-10 13:53:11 +0000

    create crypto.go for the crypto functions and remove crypto stuff from server.go

--- src/server.go
+++ src/server.go
@@ -1,21 +1,9 @@
 package main
 
 import (
-	"crypto/sha512"
 	"net/http"
 	"passwordserver/src/routes"
-
-	"golang.org/x/crypto/pbkdf2"
 )
-
-func deriveKey(email string, masterPassword string, iterations int) []byte {
-	emailBytes := []byte(email)
-	masterPasswordBytes := []byte(masterPassword)
-	keyLength := 512 / 8
-
-	dk := pbkdf2.Key(emailBytes, masterPasswordBytes, iterations, keyLength, sha512.New)
-	return dk
-}
 
 func handleRequests() {
 	http.HandleFunc("/api/v1/auth/signin", routes.SigninHandler)
@@ -25,7 +13,5 @@ func handleRequests() {
 }
 
 func main() {
-	// fmt.Print(deriveKey("email@email.email", "masterpassword", 150000))
-
 	handleRequests()
 }
