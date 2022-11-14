300ccb253820a504121e477996e8a9a6d0ebe80b
Tyler <68185249+tywil04@users.noreply.github.com>
2022-11-14 08:40:48 +0000

    db now only recongnises testing, development and production ENVIRONMENT

--- src/lib/database/database.go
+++ src/lib/database/database.go
@@ -15,10 +15,12 @@ func DatabaseConnect() {
 	var databasePath string
 	if os.Getenv("ENVIRONMENT") == "testing" {
 		databasePath = os.Getenv("TEST_DB_PATH")
-	} else if os.Getenv("ENVIRONMENT") == "dev" {
+	} else if os.Getenv("ENVIRONMENT") == "development" {
 		databasePath = os.Getenv("DEV_DB_PATH")
-	} else {
+	} else if os.Getenv("ENVIRONMENT") == "production" {
 		databasePath = os.Getenv("DB_PATH")
+	} else {
+		panic("Invalid 'ENVIRONMENT' set.")
 	}
 
 	db, dbError := gorm.Open(sqlite.Open(databasePath), &gorm.Config{})
