1332a8d138cf37b0de09b52b402265c7fc993823
Tyler <68185249+tywil04@users.noreply.github.com>
2022-11-17 11:31:50 +0000

    update cookie methods to return user/errors

--- src/lib/crypto/crypto.go
+++ src/lib/crypto/crypto.go
@@ -33,69 +33,86 @@ type SessionCookie struct {
 	SessionTokenId uuid.UUID
 }
 
-func CreateSessionCookie(response http.ResponseWriter, user database.User) {
-	privateKey, _ := rsa.GenerateKey(rand.Reader, 4096)
-	publicKey := &privateKey.PublicKey
+func CreateSessionCookie(response http.ResponseWriter, user database.User) error {
+	if database.Database != nil {
+		privateKey, _ := rsa.GenerateKey(rand.Reader, 4096)
+		publicKey := &privateKey.PublicKey
 
-	sessionToken := database.SessionToken{
-		UserId: user.Id,
-		N:      publicKey.N.Bytes(),
-		E:      publicKey.E,
+		sessionToken := database.SessionToken{
+			UserId: user.Id,
+			N:      publicKey.N.Bytes(),
+			E:      publicKey.E,
+		}
+		database.Database.Create(&sessionToken)
+		user.SessionTokens = append(user.SessionTokens, sessionToken)
+
+		sessionCookie := SessionCookie{
+			UserId:         user.Id,
+			SessionTokenId: sessionToken.Id,
+		}
+		jsonPayload := new(bytes.Buffer)
+		json.NewEncoder(jsonPayload).Encode(sessionCookie)
+		hashed := sha512.Sum512(jsonPayload.Bytes())
+
+		signature, _ := rsa.SignPKCS1v15(rand.Reader, privateKey, crypto.SHA512, hashed[:])
+
+		encodedSignature := hex.EncodeToString(signature)
+		encodedSessionCookie := hex.EncodeToString(jsonPayload.Bytes())
+
+		cookie := http.Cookie{
+			Name:     "SessionToken",
+			Value:    encodedSessionCookie + "," + encodedSignature,
+			Expires:  time.Now().Add(45784758475874),
+			Secure:   false,
+			HttpOnly: true,
+			Path:     "/",
+		}
+
+		http.SetCookie(response, &cookie)
+
+		return nil
+	} else {
+		return database.Database.Error
 	}
-	database.Database.Create(&sessionToken)
-	user.SessionTokens = append(user.SessionTokens, sessionToken)
-
-	sessionCookie := SessionCookie{
-		UserId:         user.Id,
-		SessionTokenId: sessionToken.Id,
-	}
-	jsonPayload := new(bytes.Buffer)
-	json.NewEncoder(jsonPayload).Encode(sessionCookie)
-	hashed := sha512.Sum512(jsonPayload.Bytes())
-
-	signature, _ := rsa.SignPKCS1v15(rand.Reader, privateKey, crypto.SHA512, hashed[:])
-
-	encodedSignature := hex.EncodeToString(signature)
-	encodedSessionCookie := hex.EncodeToString(jsonPayload.Bytes())
-
-	cookie := http.Cookie{
-		Name:     "SessionToken",
-		Value:    encodedSessionCookie + "," + encodedSignature,
-		Expires:  time.Now().Add(45784758475874),
-		Secure:   false,
-		HttpOnly: true,
-		Path:     "/",
-	}
-
-	http.SetCookie(response, &cookie)
 }
 
-func VerifySessionCookie(request *http.Request) bool {
-	cookie, cookieError := request.Cookie("SessionToken")
+func VerifySessionCookie(request *http.Request) (bool, database.User) {
+	if database.Database != nil {
+		cookie, cookieError := request.Cookie("SessionToken")
 
-	if cookieError != nil {
-		return false
+		if cookieError != nil {
+			return false, database.User{}
+		}
+
+		splitValue := strings.Split(cookie.Value, ",")
+		jsonSessionCookie, _ := hex.DecodeString(splitValue[0])
+
+		signature, _ := hex.DecodeString(splitValue[1])
+
+		sessionCookie := SessionCookie{}
+		json.NewDecoder(bytes.NewBuffer(jsonSessionCookie)).Decode(&sessionCookie)
+
+		sessionToken := database.SessionToken{}
+		database.Database.First(&sessionToken, "id = ?", sessionCookie.SessionTokenId, "user_id = ?", sessionCookie.UserId)
+
+		publicKey := rsa.PublicKey{
+			N: new(big.Int).SetBytes(sessionToken.N),
+			E: sessionToken.E,
+		}
+
+		jsonPayload := new(bytes.Buffer)
+		json.NewEncoder(jsonPayload).Encode(sessionCookie)
+		hashed := sha512.Sum512(jsonPayload.Bytes())
+
+		user := database.User{}
+		database.Database.First("id = ?", sessionToken.UserId)
+
+		if rsa.VerifyPKCS1v15(&publicKey, crypto.SHA512, hashed[:], signature) == nil {
+			return true, user
+		}
+
+		return false, database.User{}
+	} else {
+		return false, database.User{}
 	}
-
-	splitValue := strings.Split(cookie.Value, ",")
-	jsonSessionCookie, _ := hex.DecodeString(splitValue[0])
-
-	signature, _ := hex.DecodeString(splitValue[1])
-
-	sessionCookie := SessionCookie{}
-	json.NewDecoder(bytes.NewBuffer(jsonSessionCookie)).Decode(&sessionCookie)
-
-	sessionToken := database.SessionToken{}
-	database.Database.First(&sessionToken, "id = ?", sessionCookie.SessionTokenId, "user_id = ?", sessionCookie.UserId)
-
-	publicKey := rsa.PublicKey{
-		N: new(big.Int).SetBytes(sessionToken.N),
-		E: sessionToken.E,
-	}
-
-	jsonPayload := new(bytes.Buffer)
-	json.NewEncoder(jsonPayload).Encode(sessionCookie)
-	hashed := sha512.Sum512(jsonPayload.Bytes())
-
-	return rsa.VerifyPKCS1v15(&publicKey, crypto.SHA512, hashed[:], signature) == nil
 }
